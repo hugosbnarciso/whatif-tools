b92070fd124d32bd8de72f5be4a3ebbbd6d97d03
Hugo <hugosbnarciso@gmail.com>
2024-06-24 13:05:01 +0000

    use .env for environment variables instead of config.ini

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
-import configparser
+import os
+from dotenv import load_dotenv
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+# Load environment variables from .env file
+load_dotenv()
 
-SECRET_KEY = config.get('SECRETS', 'SECRET_KEY')
-OWM_API_KEY = config.get('SECRETS', 'OWM_API_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
+OWM_API_KEY = os.getenv('OWM_API_KEY')
 
+if not SECRET_KEY:
+    raise ValueError("No SECRET_KEY set for Flask application.")
 if not OWM_API_KEY:
     raise ValueError("No OWM API key set.")
