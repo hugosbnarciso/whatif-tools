23705eeaf6df5576f28fea6133136c7caedbd30c
hugosbnarciso <44305822+hugosbnarciso@users.noreply.github.com>
2024-08-23 14:46:46 +0530

    Changes to be committed: 	modified:   js/weather.js

--- js/weather.js
+++ js/weather.js
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
     weatherBox.innerHTML = `
             <div style="display: flex; align-items: center;  list-style: none; gap: 0.8rem;">
                 <ul>
-                    <li><img src="assets/images/weather/${icon}@2x.svg" alt="${description}" style="height: 100px" /></li>
+                    <li><img src="assets/images/weather/${icon}@2x.svg" alt="${description}" style="height: 50px;" /></li>
                 </ul>
                 <ul>
                     <li style="font-size: 2.3rem; font-weight: bold;">${temp}°C</li>
